08518914d1a57dc098c26560969f088fec11549b
asantech <moayeri68@gmail.com>
2023-11-09 18:52:23 +0330

    Feat: clean up initial code

---
+++ src/components/tools/ImageDragger.tsx
@@ -0,0 +1,11 @@
+import "./ImageDragger.scss";
+
+const ImageDragger = () => {
+  return (
+    <div className="image-dragger-box">
+      <div className="image-dragger"></div>
+    </div>
+  );
+};
+
+export default ImageDragger;
