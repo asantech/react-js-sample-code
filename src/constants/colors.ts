542a058a1a31bfc1da2621ec062f8c58823846b3
asantech <moayeri68@gmail.com>
2024-05-04 12:59:22 +0330

    Feat: add & use 'colors.ts' file

---
+++ src/constants/colors.ts
@@ -0,0 +1,3 @@
+export const colors = Object.freeze({
+    blueAlpha: "rgb(209,238,249)"
+})
--- src/pages/auth/AuthPageWrapper.tsx
+++ src/pages/auth/AuthPageWrapper.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react"
 
+import { colors } from "../../constants/colors"
+
 function AuthPageWrapper({ children }: Readonly<PropsWithChildren>) {
   return (
     <div className="grid grid-cols-12 h-screen">
       <div
         className="col-span-6 flex py-20 justify-start items-center flex-col gap-12 h-screen"
-        style={{ backgroundColor: "rgb(209,238,249)" }}
+        style={{ backgroundColor: colors.blueAlpha }}
       >
         <img className="w-96 h-96" src="/logo.jpg" alt="App Logo" />
         <div>
