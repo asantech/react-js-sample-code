e3e0a37902165552ef6c93aca582ae6a469b610a
asantech <moayeri68@gmail.com>
2024-05-17 13:54:50 +0330

    Fix: remove background of logo

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -8,12 +8,7 @@ function Sidebar({ expanded }: Readonly<SidebarProps>) {
   return (
     <aside className="h-screen bg-sky-400">
       <div className="flex justify-start items-center gap-5 p-5">
-        <img
-          className="w-16"
-          src="/logo.jpg"
-          alt="App Logo"
-          style={{ borderRadius: "50%" }}
-        />
+        <img className="w-16" src="/logo.png" alt="App Logo" />
         {expanded && (
           <span className="text-white text-xl font-semibold">
             React sample App
