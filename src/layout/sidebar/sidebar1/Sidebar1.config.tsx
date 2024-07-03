a1b8c0e2d295417b2a900f94aace59f1c46b9b92
asantech <moayeri68@gmail.com>
2024-07-04 01:13:32 +0330

    Refactor: make 'Sidebar1.config.tsx' cleaner

--- src/layout/sidebar/sidebar1/Sidebar1.config.tsx
+++ src/layout/sidebar/sidebar1/Sidebar1.config.tsx
@@ -13,170 +13,181 @@ export type SidebarMenuGroupType = {
   menu: SidebarMenuType
 }
 
+const AUTH_SIDEBAR_CONFIG = {
+  label: "auth",
+  iconPath: "",
+  linkPath: "/auth",
+  menu: [
+    {
+      label: "OAuth",
+      linkPath: "/o-auth",
+    },
+  ],
+}
+const ROUTES_SIDEBAR_CONFIG = {
+  label: "routes",
+  linkPath: "/routes",
+  iconPath: "",
+}
+const LAYOUTS_SIDEBAR_CONFIG = {
+  label: "layouts",
+  linkPath: "/layouts",
+  iconPath: "",
+  menu: [
+    {
+      label: "sidebars",
+      linkPath: "/sidebar",
+      iconPath: "",
+    },
+  ],
+}
+const COMPONENTS_SIDEBAR_CONFIG = {
+  label: "components",
+  iconPath: "",
+  linkPath: "/components",
+  menu: [
+    {
+      label: "dropdowns",
+      linkPath: "/dropdowns",
+    },
+    {
+      label: "date pickers",
+      linkPath: "/date-pickers",
+    },
+    {
+      label: "sliders",
+      linkPath: "/sliders",
+    },
+  ],
+}
+const SERVICES_SIDEBAR_CONFIG = {
+  label: "services",
+  iconPath: "",
+  linkPath: "/services",
+  menu: [
+    {
+      label: "native",
+      linkPath: "/native",
+      menu: [
+        {
+          label: "mock",
+          linkPath: "/mock",
+        },
+        {
+          label: "XMLHttpRequest",
+          linkPath: "/xml-http-request",
+        },
+        {
+          label: "fetch",
+          linkPath: "/fetch",
+        },
+      ],
+    },
+    {
+      label: "libraries",
+      linkPath: "/libraries",
+      menu: [
+        {
+          label: "axios",
+          linkPath: "/axios",
+        },
+        {
+          label: "tanstack-query",
+          linkPath: "/tanstack-query",
+        },
+      ],
+    },
+  ],
+}
+const MEDIA_SIDEBAR_CONFIG = {
+  label: "media",
+  iconPath: "",
+  linkPath: "/media",
+  menu: [
+    {
+      label: "audio",
+      linkPath: "/audio",
+      menu: [
+        {
+          label: "players",
+          linkPath: "/players",
+        },
+      ],
+    },
+    {
+      label: "video",
+      linkPath: "/video",
+      menu: [
+        {
+          label: "players",
+          linkPath: "/players",
+        },
+      ],
+    },
+  ],
+}
+const SETTINGS_SIDEBAR_CONFIG = {
+  label: "settings",
+  iconPath: "",
+  linkPath: "/settings",
+  menu: [
+    {
+      label: "profiles",
+      linkPath: "/profiles",
+    },
+    {
+      label: "sessions",
+      linkPath: "/sessions",
+    },
+    {
+      label: "themes",
+      linkPath: "/themes",
+    },
+  ],
+}
+const UI_SIDEBAR_CONFIG = {
+  label: "UI",
+  iconPath: "",
+  linkPath: "/ui",
+  menu: [
+    {
+      label: "icons",
+      linkPath: "/icons",
+    },
+  ],
+}
+const MOCKS_SIDEBAR_CONFIG = {
+  label: "Mocks",
+  iconPath: "",
+  linkPath: "/mocks",
+  menu: [
+    {
+      label: "Air Bnb",
+      linkPath: "/air-bnb",
+    },
+  ],
+}
+const Q_AND_A_SIDEBAR_CONFIG = {
+  label: "Q&A",
+  iconPath: "",
+  linkPath: "/q-and-a",
+  menu: [
+    {
+      label: "React JS",
+      linkPath: "/react-js",
+    },
+  ],
+}
+
 export const SIDEBAR_CONFIG: SidebarMenuType = [
-  {
-    label: "auth",
-    iconPath: "",
-    linkPath: "/auth",
-    menu: [
-      {
-        label: "OAuth",
-        linkPath: "/o-auth",
-      },
-    ],
-  },
-  {
-    label: "routes",
-    linkPath: "/routes",
-    iconPath: "",
-  },
-  {
-    label: "layouts",
-    linkPath: "/layouts",
-    iconPath: "",
-    menu: [
-      {
-        label: "sidebars",
-        linkPath: "/sidebar",
-        iconPath: "",
-      },
-    ],
-  },
-  {
-    label: "components",
-    iconPath: "",
-    linkPath: "/components",
-    menu: [
-      {
-        label: "dropdowns",
-        linkPath: "/dropdowns",
-      },
-      {
-        label: "date pickers",
-        linkPath: "/date-pickers",
-      },
-      {
-        label: "sliders",
-        linkPath: "/sliders",
-      },
-    ],
-  },
-  {
-    label: "services",
-    iconPath: "",
-    linkPath: "/services",
-    menu: [
-      {
-        label: "native",
-        linkPath: "/native",
-        menu: [
-          {
-            label: "mock",
-            linkPath: "/mock",
-          },
-          {
-            label: "XMLHttpRequest",
-            linkPath: "/xml-http-request",
-          },
-          {
-            label: "fetch",
-            linkPath: "/fetch",
-          },
-        ],
-      },
-      {
-        label: "libraries",
-        linkPath: "/libraries",
-        menu: [
-          {
-            label: "axios",
-            linkPath: "/axios",
-          },
-          {
-            label: "tanstack-query",
-            linkPath: "/tanstack-query",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "media",
-    iconPath: "",
-    linkPath: "/media",
-    menu: [
-      {
-        label: "audio",
-        linkPath: "/audio",
-        menu: [
-          {
-            label: "players",
-            linkPath: "/players",
-          },
-        ],
-      },
-      {
-        label: "video",
-        linkPath: "/video",
-        menu: [
-          {
-            label: "players",
-            linkPath: "/players",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "settings",
-    iconPath: "",
-    linkPath: "/settings",
-    menu: [
-      {
-        label: "profiles",
-        linkPath: "/profiles",
-      },
-      {
-        label: "sessions",
-        linkPath: "/sessions",
-      },
-      {
-        label: "themes",
-        linkPath: "/themes",
-      },
-    ],
-  },
-  {
-    label: "UI",
-    iconPath: "",
-    linkPath: "/ui",
-    menu: [
-      {
-        label: "icons",
-        linkPath: "/icons",
-      },
-    ],
-  },
-  {
-    label: "Mocks",
-    iconPath: "",
-    linkPath: "/mocks",
-    menu: [
-      {
-        label: "Air Bnb",
-        linkPath: "/air-bnb",
-      },
-    ],
-  },
-  {
-    label: "Q&A",
-    iconPath: "",
-    linkPath: "/q-and-a",
-    menu: [
-      {
-        label: "React JS",
-        linkPath: "/react-js",
-      },
-    ],
-  },
+  AUTH_SIDEBAR_CONFIG,
+  ROUTES_SIDEBAR_CONFIG,
+  LAYOUTS_SIDEBAR_CONFIG,
+  COMPONENTS_SIDEBAR_CONFIG,
+  SERVICES_SIDEBAR_CONFIG,
+  MEDIA_SIDEBAR_CONFIG,
+  SETTINGS_SIDEBAR_CONFIG,
+  UI_SIDEBAR_CONFIG,
+  MOCKS_SIDEBAR_CONFIG,
+  Q_AND_A_SIDEBAR_CONFIG,
 ]
