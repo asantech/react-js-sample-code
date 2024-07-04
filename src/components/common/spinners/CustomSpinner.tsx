c2ebcf27229636d1e0db3759633cd42cca7ca7b1
asantech <moayeri68@gmail.com>
2024-07-04 11:19:49 +0330

    Fix: correct 'CustomSpinner' UI

--- src/components/common/spinners/CustomSpinner.tsx
+++ src/components/common/spinners/CustomSpinner.tsx
@@ -1,8 +1,6 @@
 function CustomSpinner() {
   return (
-    <div className="relative rounded-full border-2 w-5 h-5 border-solid border-white animate-spin">
-      <div className="absolute rounded-full border-t-2 w-5 h-5 border-solid border-sky-200 top-0 right-0"></div>
-    </div>
+    <div className="w-5 h-5 rounded-full border-2 border-solid border-t-2 border-t-sky-500 border-gray-300 animate-spin"></div>
   )
 }
 
