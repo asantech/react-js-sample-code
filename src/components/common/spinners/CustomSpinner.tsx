7972179a7b0c8232f2623b78e5df93a431a18936
asantech <moayeri68@gmail.com>
2024-07-04 13:21:00 +0330

    Feat: make 'CustomSpinner' color customizable

--- src/components/common/spinners/CustomSpinner.tsx
+++ src/components/common/spinners/CustomSpinner.tsx
@@ -1,6 +1,19 @@
-function CustomSpinner() {
+import clsx from "clsx"
+
+type CustomSpinnerProps = {
+  addedClassName: string
+}
+
+function CustomSpinner({
+  addedClassName = "border-t-sky-500",
+}: CustomSpinnerProps) {
   return (
-    <div className="w-5 h-5 rounded-full border-2 border-solid border-t-2 border-t-sky-500 border-gray-300 animate-spin"></div>
+    <div
+      className={clsx(
+        "w-5 h-5 rounded-full border-2 border-solid border-t-2 border-gray-300 animate-spin",
+        addedClassName
+      )}
+    ></div>
   )
 }
 
