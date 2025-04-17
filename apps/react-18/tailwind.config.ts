2d47eed08ddff2cfa11a40390d8de16cf1aae940
m.moayeri <m.moayeri@dotin.ir>
2025-04-17 16:39:55 +0330

    Feat: remove some of unnecessary 'tailwind' library & other files

--- apps/react-18/tailwind.config.ts
+++ apps/react-18/tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "tailwindcss"
-import sharedConfig from "../../packages/tailwind-theme/tailwind.config"
 
 const config: Pick<Config, "content" | "presets"> = {
   content: ["./src/**/*.{ts,tsx,jsx}"],
-  presets: [sharedConfig],
 }
 
 export default config
